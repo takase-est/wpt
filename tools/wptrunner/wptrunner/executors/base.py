56579111ca42fefc6d1f8d775aaf403719a49e60
James Graham <james@hoppipolla.co.uk>
2019-10-31 12:46:33 +0000

    Add load method to protocol

--- tools/wptrunner/wptrunner/executors/base.py
+++ tools/wptrunner/wptrunner/executors/base.py
@@ -561,6 +561,9 @@ class WdspecRun(object):
 
 
 class ConnectionlessBaseProtocolPart(BaseProtocolPart):
+    def load(self, url):
+        pass
+
     def execute_script(self, script, asynchronous=False):
         pass
 
